10cf529e81ed7e0748b2475c9e2401e56684b937
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-08-25 22:07:35 -0300

    add description about me and button cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,28 +1,39 @@
-import React from 'react'
+import React from "react";
 
-import './About.css'
+import "./About.css";
+import { Button, ButtonToolbar } from "react-bootstrap";
 
-import Photo from '../img/me.jpeg'
+import Photo from "../img/me.jpeg";
 
-const About = props =>{
+const About = props => {
+  return (
+    <div className="About">
+      <h3>About me</h3>
 
-    return(
-        <div className="About">
-            <h3>About me</h3>
-
-            <div className="under-line">
-                <div className="line-up">
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-            </div>
-
-            <img src={Photo} className="my-photo"/>
-
-            <p>I'm front-end developer and computer sciece student</p>
+      <div className="under-line">
+        <div className="line-up">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
         </div>
-    )
-}
+      </div>
 
-export default About
+      <img src={Photo} className="my-photo" />
+
+      <div className="description">
+        <p>
+          Graduating in Computer Science, Front-end Developer, Javascript
+          ecosystem. I've been working with that since my 16 years old, I worked
+          as Freelancer and some startups developing web applications. Now I
+          also give talks about my knowledge throughout my city.
+        </p>
+      </div>
+
+      <ButtonToolbar>
+        <Button variant="primary" className="text-button">DOWNLOAD MY CV</Button>
+      </ButtonToolbar>
+    </div>
+  );
+};
+
+export default About;
