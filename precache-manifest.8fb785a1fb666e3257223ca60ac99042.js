8514073891f1d0b96f0d9af6af3403b5cca001a1
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-09-05 15:07:09 -0300

    Updates

---
+++ precache-manifest.8fb785a1fb666e3257223ca60ac99042.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2cbdd4ec7cb996bbd88056e74f1817",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bd14223f49d0a6d3ce7d",
+    "url": "/portfolio/static/css/2.a8a26b3a.chunk.css"
+  },
+  {
+    "revision": "c64d2cb38575c5ca1fae",
+    "url": "/portfolio/static/css/main.3ddd6631.chunk.css"
+  },
+  {
+    "revision": "bd14223f49d0a6d3ce7d",
+    "url": "/portfolio/static/js/2.109d3274.chunk.js"
+  },
+  {
+    "revision": "c64d2cb38575c5ca1fae",
+    "url": "/portfolio/static/js/main.42fe6b80.chunk.js"
+  },
+  {
+    "revision": "974759e2920968b150db",
+    "url": "/portfolio/static/js/runtime~main.5b9b1ed7.js"
+  },
+  {
+    "revision": "bb82ed2fcd6ed950dd584041e12e6cbc",
+    "url": "/portfolio/static/media/back.bb82ed2f.jpg"
+  },
+  {
+    "revision": "599419a1e61d3b532a3efdabe9d3a2b0",
+    "url": "/portfolio/static/media/github.599419a1.svg"
+  },
+  {
+    "revision": "7dc65b2f3b1de6977779f291852029b5",
+    "url": "/portfolio/static/media/instagram.7dc65b2f.svg"
+  },
+  {
+    "revision": "441a8e60db4a8db2b4cb357302db6508",
+    "url": "/portfolio/static/media/linkedin.441a8e60.svg"
+  },
+  {
+    "revision": "1f4dd7c8b9e3896422f4e6b6b8fec1d9",
+    "url": "/portfolio/static/media/me.1f4dd7c8.jpeg"
+  },
+  {
+    "revision": "01c8243d53abf90b2ca3caac4c11f5e2",
+    "url": "/portfolio/static/media/twitter.01c8243d.svg"
+  },
+  {
+    "revision": "298ae0fbecaa380048c33bf3ae06f5f5",
+    "url": "/portfolio/static/media/youtube.298ae0fb.svg"
+  }
+]);
