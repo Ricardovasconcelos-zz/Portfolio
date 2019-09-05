71080d67b273ddeac5e90501739aad8b402d1e01
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-09-05 15:01:35 -0300

    gh_pages

---
+++ build/precache-manifest.3575054785abcc8398ad651a534a1491.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87ef148f4206c26aac1fe75aea60d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e80b7922ed82295a49",
+    "url": "/static/css/2.c6af0b9d.chunk.css"
+  },
+  {
+    "revision": "73515fcfdc03bd2c0299",
+    "url": "/static/css/main.3e9d6873.chunk.css"
+  },
+  {
+    "revision": "64e80b7922ed82295a49",
+    "url": "/static/js/2.539211ed.chunk.js"
+  },
+  {
+    "revision": "73515fcfdc03bd2c0299",
+    "url": "/static/js/main.aaae02a4.chunk.js"
+  },
+  {
+    "revision": "338270fb3720bf7e1cd6",
+    "url": "/static/js/runtime~main.ace06e6b.js"
+  },
+  {
+    "revision": "bb82ed2fcd6ed950dd584041e12e6cbc",
+    "url": "/static/media/back.bb82ed2f.jpg"
+  },
+  {
+    "revision": "599419a1e61d3b532a3efdabe9d3a2b0",
+    "url": "/static/media/github.599419a1.svg"
+  },
+  {
+    "revision": "7dc65b2f3b1de6977779f291852029b5",
+    "url": "/static/media/instagram.7dc65b2f.svg"
+  },
+  {
+    "revision": "441a8e60db4a8db2b4cb357302db6508",
+    "url": "/static/media/linkedin.441a8e60.svg"
+  },
+  {
+    "revision": "1f4dd7c8b9e3896422f4e6b6b8fec1d9",
+    "url": "/static/media/me.1f4dd7c8.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "01c8243d53abf90b2ca3caac4c11f5e2",
+    "url": "/static/media/twitter.01c8243d.svg"
+  },
+  {
+    "revision": "298ae0fbecaa380048c33bf3ae06f5f5",
+    "url": "/static/media/youtube.298ae0fb.svg"
+  }
+]);
